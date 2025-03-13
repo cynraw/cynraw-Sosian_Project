18ed9a80e2c3efccb15296fd46ba4e31715029f3
Cynthia Ronoh <cynthiaronoh24@gmail.com>
2025-03-13 23:14:46 +0300

    frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: "./", 
   server: {
     proxy: {
       "/api": {
         target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
   build: {
-    outDir: "dist", 
+    outDir: "dist",
+    emptyOutDir: true,  
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"), 
+      },
+    },
   },
 });
-
